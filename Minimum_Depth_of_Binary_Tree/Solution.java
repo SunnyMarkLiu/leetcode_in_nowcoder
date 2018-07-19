fb53f754d6d0a681a93d5b5af7c022237aa60cf4
sunnymarkliu <sunnymarkliu101@gmail.com>
2018-07-19 10:28:46 +0800

    add some problems

--- Minimum_Depth_of_Binary_Tree/Solution.java
+++ Minimum_Depth_of_Binary_Tree/Solution.java
@@ -1,6 +1,9 @@
 
 package Minimum_Depth_of_Binary_Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -20,18 +23,55 @@ public class Solution {
         TreeNode(int x) { val = x; }
     }
 
-    /*
-    递归的方式实现, 比较左右子树的深度
+    /**
+     * 递归的方式实现, 比较左右子树的深度
      */
-    public int run(TreeNode root) {
+    public int runRecursion(TreeNode root) {
         if (root == null)
             return 0;
 
         // 如果 root 的左右子树都存在，则递归获取左右子树的 min + 1
         if (root.left != null && root.right != null)
-            return Math.min(run(root.left), run(root.right)) + 1;
+            return Math.min(runRecursion(root.left), runRecursion(root.right)) + 1;
 
         // 如果当前结点只存在一个左右结点，选择有叶子结点的分支，即 depth 较大的，同时注意加上当前的结点 + 1
-        return Math.max(run(root.left), run(root.right)) + 1;
+        return Math.max(runRecursion(root.left), runRecursion(root.right)) + 1;
+    }
+
+    /**
+     * 层次遍历，找到第一个左右节点都是 null 的叶子节点，直接返回此时的 depth
+     */
+    public int run(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return 1;
+
+        int depth = 0;
+        // 层次遍历，先进先出，采用队列
+        Queue<TreeNode> levelQueue = new LinkedList<>();
+        levelQueue.offer(root);
+
+        while (!levelQueue.isEmpty()) {
+            // 当前待遍历 level 的节点数
+            int cur_level_size = levelQueue.size();
+            depth++;
+
+            // 遍历当前层
+            for (int i=0; i < cur_level_size; i++) {
+                TreeNode node = levelQueue.poll();
+                // 如果此 node 为叶子节点，直接返回此时的 depth
+                if (node.left == null && node.right == null)
+                    return depth;
+
+                if (node.left != null)
+                    levelQueue.offer(node.left);
+
+                if (node.right != null)
+                    levelQueue.offer(node.right);
+            }
+        }
+        return 0;
     }
 }
--- Minimum_Depth_of_Binary_Tree/Solution.py
+++ Minimum_Depth_of_Binary_Tree/Solution.py
@@ -15,17 +15,46 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def minDepth(self, root):
+    def minDepthRecursion(self, root):
         """
-        :type root: TreeNode
-        :rtype: int
+        递归的方式实现, 比较左右子树的深度
         """
         if not root:
             return 0
 
         # 如果 root 的左右子树都存在，则递归获取左右子树的 min + 1
         if root.left and root.right:
-            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+            return min(self.minDepthRecursion(root.left), self.minDepthRecursion(root.right)) + 1
 
         # 如果当前结点只存在一个左右结点，选择有叶子结点的分支，即 depth 较大的，同时注意加上当前的结点 + 1
-        return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        return max(self.minDepthRecursion(root.left), self.minDepthRecursion(root.right)) + 1
+
+    def minDepth(self, root):
+        """
+        层次遍历，找到第一个左右节点都是 null 的叶子节点，直接返回此时的 depth
+        """
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return 1
+
+        depth = 0
+        levelQueue = [root]
+
+        while len(levelQueue) > 0:
+            cur_level_size = len(levelQueue)
+            depth += 1
+
+            for i in range(cur_level_size):
+                node = levelQueue.pop(0)
+                if not node.left and not node.right:
+                    return depth
+
+                if node.left:
+                    levelQueue.append(node.left)
+
+                if node.right:
+                    levelQueue.append(node.right)
+
+        return 0
